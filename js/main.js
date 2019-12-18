769706d48f9f37e1873ad18d3ba8f1e3ceb333ab
Amber M <amartineau02@wildcats.jwu.edu>
2019-12-18 16:13:21 -0500

    home fix
    Added styling to the home page.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,9 @@
    Function for retrieving the main weather info
    ----------------------------------------------- */
 
-// Replace the lat/long below with the lat/long for your desired location.
-// Get your city lat/long using https://www.latlong.net/
+
+
+// Replace the lat/long below with the lat/long for your desired location. Get your city lat/long using https://www.latlong.net/
 var latlong = '38.046360,-84.497017';
 
 // Your unique API key. Place the long string of characters between the quotes.
@@ -12,111 +13,59 @@ var apikey = '9c200e867556081d48f46545f9eba2ad';
 // Access the DarkSky API for weather data. DO NOT EDIT THIS LINE.
 $.getJSON('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+apikey+'/' + latlong)
 
+
+
 // Display the weather content once it is loaded, not before.
 .done(function(forecast) {
-	// Your header section covers over the main weather info.
-	// When the data is available, you will need to hide/remove this section
-	// in order to see the main content. How you do this is up to you.
-	
-	// The most basic basic approach is to just hide the header (uncomment to use)
-		
-	// $('header').hide();
-	
-	// Other methods include animating the header away.
-	// This can be done by adding a class name containing CSS animation
-	// code to the header like this (uncomment to use)
 
-	// $('header').addClass('anim');
+	$('header button').click(function(){
+	  $('header').hide();
+	});
 
-	// This assumes you have written a class with the animation code
-	// and named it .anim
-
-	// Another way to remove the header is to provide a button and the event
-	// to trigger what happens when the button is clicked (uncomment to use)
-
-	// $('header').append('<a class="button" href="#">Click</a>');
-	// $('header .button').click(function(){
-	//   $('header').hide();
-	// });
-
-	// The following line calls a function to display
-	// the main weather information. DO NOT EDIT THIS LINE.
+	// The following line calls a function to display the main weather information. DO NOT EDIT THIS LINE.
 	displayData(forecast);
 })
 
-// Print the data object feturned from DarkSky for all the details
-// DO NOT EDIT THIS LINE.
+
+
+// Print the data object feturned from DarkSky for all the details. DO NOT EDIT THIS LINE.
 .always(function(forecast) {
 	console.log(forecast);
 });
+
 
 
 /* -----------------------------------------------
    Function for displaying the main weather info
    ----------------------------------------------- */
 
-// All of the data comes from the "forecast" object returned
-// from the DarkSky API. Inspect this page in the browser
-// for a full list of data that can be used using the methods below.
+// All of the data comes from the "forecast" object returned from the DarkSky API.
+// Inspect this page in the browser for a full list of data that can be used using the methods below.
+
 
 function displayData(forecast){
 
-	// Target an element in your interface and display
-	// dynamic weather information inside of it
+	$('.weather .icon').append('<img src="'+displayIcon(forecast.daily.data[0].icon)+'">');
 
-	// All of the data you need is located in the "Console" tab
-	// when you inspect th code in the browser
-	// Click the arrow next to the "Object" to drill down into the data
-	// You can reference these data points in your code using the following
-	// method(s).
+	$('.weather .detail h1').html(forecast.daily.data[0].temperatureHigh);
+	$('.weather .detail h1').html(Math.round(forecast.daily.data[0].temperatureHigh));
 
-	// For example, if I have an element <div class="today"> in my main content area
-	// I can add data from the "Daily" array like this
+	$('.weather .detail p1').html(forecast.daily.data[0].summary);
 
-	$('.day01 p').html(forecast.daily.data[0].temperatureHigh);
-	$('.day02 p').html(forecast.daily.data[1].temperatureHigh);
-	$('.day03 p').html(forecast.daily.data[2].temperatureHigh);
-	$('.day04 p').html(forecast.daily.data[3].temperatureHigh);
-	
-	// In this example, the high temperature for the first day of the week
-	// (referenced by the number 0) is written as HTML inside the <div class="today"> element
-	// If I want to round this number up, I would modify the code like this
+	$('.weather .detail .high').html(forecast.daily.data[0].temperatureHigh);
+	$('.weather .detail .high').html(Math.round(forecast.daily.data[0].temperatureHigh));
 
-	$('.day01 p').html(Math.round(forecast.daily.data[0].temperatureHigh));
-	$('.day01').append('<img src="'+displayIcon(forecast.daily.data[0].icon)+'">');
+	$('.weather .detail .low').html(forecast.daily.data[0].temperatureLow);
+	$('.weather .detail .low').html(Math.round(forecast.daily.data[0].temperatureLow));
 
-	$('.day02 p').html(Math.round(forecast.daily.data[1].temperatureHigh));
-	$('.day02 h3').html(displayDay(1));
+	$('.weather .detail .humidity').html(forecast.daily.data[0].humidity);
+	$('.weather .detail .humidity').html(Math.round(forecast.daily.data[0].humidity));
 
-	$('.day03 p').html(Math.round(forecast.daily.data[2].temperatureHigh));
-	$('.day03 h3').html(displayDay(2));
+	$('.weather .detail .winds').html(forecast.daily.data[0].windSpeed);
+	$('.weather .detail .winds').html(Math.round(forecast.daily.data[0].windSpeed));
 
-	$('.day04 p').html(Math.round(forecast.daily.data[3].temperatureHigh));
-	$('.day04 h3').html(displayDay(3));
-
-	// If I want to display the same information for tomorrow, change the 0 to 1
-
-	// $('.today').html(Math.round(forecast.daily.data[1].temperatureHigh));
-
-	// If I want to display a summary of the weather
-	// (like, "scattered thundershowers...") for today
-
-	// $('.today').html(forecast.daily.data[0].summary);
-
-	// If I want to modify the display of the page element based on the weather
-	// I can access the "icon" property. This returns a value that can be used
-	// as a CSS class name that you can create with your style details
-
-	// $('.today').addClass(forecast.daily.data[0].icon);
-
-	// Note – the value of "icon" above needs to be checked in the inspect
-	// panel. It may say "rain". If this is the case, you could create a rule
-	// inside your CSS called .rain and then add, maybe, a background color
-	// or image. The full range of weather values returned by the "icon" property are
-
-	// "clear-day", "clear-night", "rain", "snow", "sleet", "wind", "fog",
-	// "cloudy", "partly-cloudy-day", "partly-cloudy-night", "hail",
-	// "thunderstorm" and "tornado"
+	$('.weather .detail .precip').html(forecast.daily.data[0].precipProbability);
+	$('.weather .detail .precip').html(Math.round(forecast.daily.data[0].precipProbability));
 
 }
 
@@ -235,3 +184,60 @@ function displayIcon(n){
 	}
 }
 
+// ----------------------------------------------
+// ALL COMMENTS FROM EARLIER IN THE CODE
+// USED FOR REFERENCE & ORGANIZATION
+// ----------------------------------------------
+
+// Your header section covers over the main weather info.
+	// When the data is available, you will need to hide/remove this section
+	// in order to see the main content. How you do this is up to you.
+	// The most basic basic approach is to just hide the header (uncomment to use)
+		// $('header').hide();
+	
+	// Other methods include animating the header away.
+	// This can be done by adding a class name containing CSS animation
+	// code to the header like this (uncomment to use)
+		// $('header').addClass('anim');
+	// This assumes you have written a class with the animation code
+	// and named it .anim
+
+	// Another way to remove the header is to provide a button and the event
+	// to trigger what happens when the button is clicked (uncomment to use)
+	// $('header').append('<a class="button" href="#">Click</a>');
+	// $('header .button').click(function(){
+	//   $('header').hide();
+	// });
+
+
+
+	// Target an element in your interface and display dynamic weather information inside of it.
+
+	// All of the data you need is located in the "Console" tab when you inspect th code in the browser.
+	// Click the arrow next to the "Object" to drill down into the data.
+	// You can reference these data points in your code using the following method(s).
+
+	// For example, if I have an element <div class="today"> in my main content area
+	// I can add data from the "Daily" array like this.
+
+
+
+	// If I want to display the same information for tomorrow, change the 0 to 1
+		// $('.today').html(Math.round(forecast.daily.data[1].temperatureHigh));
+
+	// If I want to display a summary of the weather
+	// (like, "scattered thundershowers...") for today
+		// $('.today').html(forecast.daily.data[0].summary);
+
+		// If I want to modify the display of the page element based on the weather
+	// I can access the "icon" property. This returns a value that can be used
+	// as a CSS class name that you can create with your style details
+		// $('.today').addClass(forecast.daily.data[0].icon);
+
+	// Note – the value of "icon" above needs to be checked in the inspect
+	// panel. It may say "rain". If this is the case, you could create a rule
+	// inside your CSS called .rain and then add, maybe, a background color
+	// or image. The full range of weather values returned by the "icon" property are
+		// "clear-day", "clear-night", "rain", "snow", "sleet", "wind", "fog",
+		// "cloudy", "partly-cloudy-day", "partly-cloudy-night", "hail",
+		// "thunderstorm" and "tornado"
